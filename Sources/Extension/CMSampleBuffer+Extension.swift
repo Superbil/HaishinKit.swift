e674a39a5b9a491c7da7ad5bc6e2c96d2d301a2f
Superbil <superbil@gmail.com>
2022-03-11 18:11:50 +0800

    Add image extension on CMSampleBuffer

--- Sources/Extension/CMSampleBuffer+Extension.swift
+++ Sources/Extension/CMSampleBuffer+Extension.swift
@@ -102,6 +102,33 @@ extension CMSampleBuffer {
         )
     }
 
+    /// https://stackoverflow.com/questions/15726761/make-an-uiimage-from-a-cmsamplebuffer
+    func image(orientation: UIImage.Orientation = .up, scale: CGFloat = 1.0) -> UIImage? {
+        if let buffer = CMSampleBufferGetImageBuffer(self) {
+            let ciImage = CIImage(cvPixelBuffer: buffer)
+
+            return UIImage(ciImage: ciImage, scale: scale, orientation: orientation)
+        }
+
+        return nil
+    }
+
+    func imageWithCGImage(orientation: UIImage.Orientation = .up, scale: CGFloat = 1.0) -> UIImage? {
+        if let buffer = CMSampleBufferGetImageBuffer(self) {
+            let ciImage = CIImage(cvPixelBuffer: buffer)
+
+            let context = CIContext(options: nil)
+
+            guard let cg = context.createCGImage(ciImage, from: ciImage.extent) else {
+                return nil
+            }
+
+            return UIImage(cgImage: cg, scale: scale, orientation: orientation)
+        }
+
+        return nil
+    }
+
     #if os(macOS)
 
     /* Used code from the example https://developer.apple.com/documentation/accelerate/vimage/reading_from_and_writing_to_core_video_pixel_buffers */
